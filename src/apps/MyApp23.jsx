d5180fb3bb2633b4c8f8963c33d5605540834775
Lemonstew <colt4949@naver.com>
2024-11-06 16:21:21 +0900

    style prop

---
+++ src/apps/MyApp23.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyApp23(props) {
+  return (
+    <div>
+      <h1 style={{color: "yellow"; backgroundColor: "black}}>Lorem ipsum dolor.</h1>
+      <p>Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit.
+        Ab doloremque eligendi est ex illo voluptates.
+        Amet cupiditate dolores ea facilis ipsa
+        iusto placeat porro quia suscipit ullam?
+        Consectetur, consequuntur, nobis.</p>
+    </div>
+  );
+}
+
+export default MyApp23;
