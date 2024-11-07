731b21feef5d890dce42fd24436da80e289adf61
Lemonstew <colt4949@naver.com>
2024-11-07 11:17:32 +0900

    pagination

---
+++ src/apps/MyApp29.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  PaginationItems,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "../components/ui/pagination.jsx";
+import { HStack } from "@chakra-ui/react";
+
+function MyApp29(props) {
+  return (
+    <div>
+      <h5>pagination</h5>
+      <PaginationRoot count={1024} pageSize={10}>
+        <HStack>
+          <PaginationPrevTrigger />
+          <PaginationItems />
+          <PaginationNextTrigger />
+        </HStack>
+      </PaginationRoot>
+    </div>
+  );
+}
+
+export default MyApp29;
